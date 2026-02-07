f55a3282c081a1e47ca88f20960d958759722e71
Jakub Pawlowski <jakub.pawlowski00@gmail.com>
2026-02-07 22:44:51 +0100

    Replace .js config files with .ts

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,8 +1,9 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+import type { Config } from "@sveltejs/kit";
+
+const config: Config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
